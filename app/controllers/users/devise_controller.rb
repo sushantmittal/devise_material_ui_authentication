f44c85ad7fc592f893ac93d169b6231e40a7973f
Sushant Mittal <sushant@bigbinary.com>
2022-07-21 21:22:56 +0530

    Added ability to signup or login through json api.

--- app/controllers/users/devise_controller.rb
+++ app/controllers/users/devise_controller.rb
@@ -1,4 +1,6 @@
 class Users::DeviseController < ApplicationController
+  skip_before_action :verify_authenticity_token, if: -> { request.format.json? }
+
   class Responder < ActionController::Responder
     def to_turbo_stream
       controller.render(options.merge(formats: :html))
@@ -14,5 +16,5 @@ class Users::DeviseController < ApplicationController
   end
 
   self.responder = Responder
-  respond_to :html, :turbo_stream
+  respond_to :html, :turbo_stream, :json
 end
--- db/migrate/20220721065157_add_referral_columns_to_users.rb
+++ db/migrate/20220721065157_add_referral_columns_to_users.rb
@@ -3,7 +3,6 @@ class AddReferralColumnsToUsers < ActiveRecord::Migration[7.0]
     add_column :users, :referral_token, :string, null: false
     add_reference :users, :referred_by, foreign_key: { to_table: :users, on_delete: :nullify }
 
-
     add_index :users, :referral_token, unique: true
   end
 end
